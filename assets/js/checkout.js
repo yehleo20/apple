91763f8f4def02deb1bd5f02f4040cfcfb17c54a
yehmingte <iamleo0727@qq.com>
2024-01-24 10:16:27 +0800

    image

--- assets/js/checkout.js
+++
@@ -1,9 +0,0 @@
-simpleCart.ready(function () {
-  simpleCart.each(function (item, x) {
-    $('form').append('<input type="hidden" name="' + item.get('id') + ' Name" value="' + item.get('name') + '">');
-    $('form').append('<input type="hidden" name="' + item.get('id') + ' Date" value="' + item.get('date') + '">');
-    $('form').append('<input type="hidden" name="' + item.get('id') + ' Session" value="' + item.get('session') + '">');
-    $('form').append('<input type="hidden" name="' + item.get('id') + ' Total" value="' + item.get('total') + '">');
-  });
-});
-
